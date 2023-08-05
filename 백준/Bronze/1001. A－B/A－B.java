7b5756021388c03143390f9434a43e95ced045a5
ms <43956716+minsu11@users.noreply.github.com>
2023-08-05 16:16:45 +0900

    [Bronze V] Title: A-B, Time: 212 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number1 = scan.nextInt();
+        int number2 = scan.nextInt();
+        System.out.println(number1 - number2);
+    }
+}
