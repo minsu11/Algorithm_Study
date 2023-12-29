cd2934ccce73b57fae6c630698377794d13c1eb9
ms <43956716+minsu11@users.noreply.github.com>
2023-12-29 16:09:18 +0900

    [level unrated] Title: 짝수의 합, Time: 0.02 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i =0; i<=n;){
+            answer += i;
+            i+=2;
+        }
+        return answer;
+    }
+}
