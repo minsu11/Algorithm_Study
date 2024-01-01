9de8031e33164c3da59e9d38062c73aef2aae337
ms <43956716+minsu11@users.noreply.github.com>
2024-01-01 15:20:49 +0900

    [level unrated] Title: 뒤에서 5등 위로, Time: 0.03 ms, Memory: 64.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length - 5];
+        for(int i = 0; i < num_list.length; i++){
+            for(int j = i; j < num_list.length; j++){
+                if(num_list[i]>num_list[j]){
+                    int tmp = num_list[i];
+                    num_list[i] = num_list[j];
+                    num_list[j] = tmp;
+                }
+            }
+        }
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = num_list[i+5];
+        }
+        
+        return answer;
+    }
+}
