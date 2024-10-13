baee285c020b173b4d9ddf183a610c73df2362f5
ms <43956716+minsu11@users.noreply.github.com>
2024-10-13 15:55:22 +0900

    [Bronze II] Title: 분해합, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- Java/백준/Bronze/2231. 분해합/분해합.java
+++ Java/백준/Bronze/2231. 분해합/분해합.java
@@ -1,35 +1,30 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-            String str = br.readLine();
-            int n = Integer.parseInt(str);
-            int result = 0;
-
-            // n의 가장 작은 생성자는 max(1, n - 9 * 자릿수)부터 시작
-            for(int i = Math.max(1, n - 9 * str.length()); i < n; i++){
-                int sum = i;
-                int temp = i;
-
-                while(temp > 0){
-                    sum += temp % 10; // 각 자리수 더하기
-                    temp /= 10;
-                }
-
-                if(sum == n){
-                    result = i;
-                    break;
-                }
+      try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+        String str = br.readLine();
+        int n = Integer.parseInt(str);
+        for(int i = Math.max(1,n-9 *str.length()); i <n; i++ ){
+            int z = i;
+            int result = i;
+            while(z != 0){
+                result += z % 10;
+                z /= 10;
             }
-
-            System.out.println(result == 0 ? 0 : result);
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            if(result == n){
+                System.out.println(i);
+                return;
+            }
         }
+          System.out.println("0");
+
+      }catch (IOException e){}
     }
 }
