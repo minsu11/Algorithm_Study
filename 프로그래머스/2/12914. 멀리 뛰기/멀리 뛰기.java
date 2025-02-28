e3f0da7533199bc99b3e5d01b065e4a6bd4e61f1
ms <43956716+minsu11@users.noreply.github.com>
2025-02-28 13:25:26 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long solution(int n) {
+        if(n ==1){
+            return 1;
+        }
+        else if(n == 2){
+            return 2;
+        }
+        long x1 = 1l;
+        long x2 = 2l;
+        long answer = 0;
+        for(int i = 2; i < n; i++){
+            answer = x1 + x2;
+            x1 = x2 % 1234567;
+            x2 = answer%1234567;
+        }
+        
+        return answer%1234567;
+    }
+    
+    
+}
