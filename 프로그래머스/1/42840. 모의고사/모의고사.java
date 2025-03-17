10f781a87dcd3d9649d52b2685d6f12e3f09e2b8
ms <43956716+minsu11@users.noreply.github.com>
2025-03-17 17:46:57 +0900

    [level 1] Title: 모의고사, Time: 1.40 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,57 @@
+class Solution {
+    int[] math1 = {1,2,3,4,5};
+    int[] math2 = {2,1,2,3,2,4,2,5};
+    int[] math3 = {3,3,1,1,2,2,4,4,5,5};
+    public int[] solution(int[] answers) {
+        int x1 = cal(answers, math1);
+        int x2 = cal(answers, math2);
+        int x3 = cal(answers, math3);
+        int[] answer = null;        
+        if(x1==x2 && x2 == x3){
+            answer = new int[3];
+            for(int i = 0; i < 3; i++){
+                answer[i] = i +1;
+            }
+            return answer;
+        }
+        
+        if(x1 < x2 && x2 == x3){
+            answer = new int[2];
+            answer[0] = 2;answer[1] = 3;
+            
+        }else if(x2 < x3 && x3 == x1){
+            
+            answer = new int[2];
+            answer[0] = 1; answer[1] = 3;
+        }else if(x3< x1 && x1 == x2){
+            
+            answer = new int[2];
+            answer[0] = 1; answer[1] = 2;
+        }else if(x2 < x1 && x3 < x1){
+            answer = new int[1];answer[0] = 1;
+        }else if (x1< x2 && x3 < x2){
+            answer = new int[1];answer[0] = 2;
+        }else if(x2 < x3 && x1 < x3){
+            answer = new int[1]; answer[0] = 3;
+        }
+        
+        System.out.println(x1);
+        System.out.println(x2);
+        System.out.println(x3);
+        
+        
+        return answer;
+    }
+    
+    
+    public int cal(int[] answers, int[] mathFail){
+        int result = 0;
+        for(int i = 0; i < answers.length; i++){
+            if(answers[i] == mathFail[i % mathFail.length]){
+                result++;
+            }
+            
+        }
+        return result;
+    }
+}
