f8ab70db834faf0e17332fe341e2c77847f5a4ad
ms <43956716+minsu11@users.noreply.github.com>
2025-03-18 15:41:08 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -2,16 +2,10 @@ class Solution {
     String[] days = {"THU", "FRI", "SAT","SUN","MON","TUE", "WED"  };
     int[] monthDay = {31,29,31,30,31,30,31,31,30,31,30,31};
     public String solution(int a, int b) {
-        String answer = "";
-        int start =5;
         int sum = b;
         for(int i =0; i < a-1; i++){
             sum += monthDay[i];
         }
-        
-        
-        
-        
         return days[sum%7];
     }
 }
