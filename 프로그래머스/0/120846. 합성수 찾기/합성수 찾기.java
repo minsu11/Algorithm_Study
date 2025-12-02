ea42e0055362c8b48072b9e40bbb28f52f73b12b
ms <43956716+minsu11@users.noreply.github.com>
2025-12-03 07:56:59 +0900

    [level 0] Title: 합성수 찾기, Time: 0.04 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -1,25 +1,18 @@
 class Solution {
     public int solution(int n) {
-        if(n < 4){
-            return 0;
-        }
-        int answer = 3;
+        int answer = 0;
         
         for(int i = 4; i <= n; i++){
             int cnt = 0;
-            for(int j = 1; j <i; j++){
-                if(cnt >1){
+            for(int j = 2; j <i; j++){
+                
+                if(i % j == 0){
+                    answer++;
                     break;
                 }
-                if(i % j == 0){
-                    cnt++;
-                }
-            }
-            if(cnt == 1){
-                answer++;
             }
         }
         
-        return n-answer;
+        return answer;
     }
 }
