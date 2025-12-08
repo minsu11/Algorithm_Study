f68f499debde53af108ed70a5d1a963dd0f77e9b
ms <43956716+minsu11@users.noreply.github.com>
2025-12-08 16:38:25 +0900

    [level 0] Title: A로 B 만들기, Time: 0.87 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -2,20 +2,12 @@ import java.util.*;
 class Solution {
     public int solution(String before, String after) {
 
-        String[] beforeArr = before.split("");
-        String[] afterArr = after.split("");
+        char[] beforeArr = before.toCharArray();
+        char[] afterArr = after.toCharArray();
         
         Arrays.sort(beforeArr);
         Arrays.sort(afterArr);
-        
-        for(int i = 0; i < beforeArr.length; i++){
-            if(!beforeArr[i].equals(afterArr[i])){
-                return 0;
-            }
-        }
 
-            
-
-        return 1;
+        return new String(beforeArr).equals(new String(afterArr)) ? 1 : 0;
     }
 }
