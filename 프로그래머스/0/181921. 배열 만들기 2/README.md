91223c0b1118bcca5be74c43ed78090b11cb0f86
ms <43956716+minsu11@users.noreply.github.com>
2025-12-13 15:19:05 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.28 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/README.md
+++ 프로그래머스/0/181921. 배열 만들기 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 2 - 181921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181921) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181921#) 
 
 ### 성능 요약
 
-메모리: 89 MB, 시간: 0.30 ms
+메모리: 76.5 MB, 시간: 0.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 13일 15:14:39
+2025년 12월 13일 15:19:03
 
 ### 문제 설명
 
--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -3,27 +3,21 @@ class Solution {
     public int[] solution(int l, int r) {
         List<Integer> list = new ArrayList<>();
         int first = 0;
-        int idx =0;
-        for(int i = 1; first < l ;i++){
-            first = Integer.parseInt(Integer.toBinaryString(i)) *5;
-            idx = i;
-        }
-        
-        if(first > r){
-            return new int[]{-1};
-        }else if (first == r){
-            return new int[]{first};
-        }
-        
-        
-        for(int i = idx; ; i++){
-            first = Integer.parseInt(Integer.toBinaryString(i)) *5;
+        int idx =1;
+        while(true){
+            first = Integer.parseInt(Integer.toBinaryString(idx)) *5;
+            idx++;
             if(first > r){
                 break;
             }
-            list.add(first);
+            else if(l <= first && first <= r){
+                list.add(first);    
+            }
+            
+        }   
+        if(list.size() == 0){
+            return new int[]{-1};
         }
-        
         
         int[] answer = new int[list.size()];
         for(int i = 0; i < list.size(); i++){
