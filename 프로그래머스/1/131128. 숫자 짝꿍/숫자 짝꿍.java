648b44f77e3f9fc04c367aadfc60e15105d7cfa8
ms <43956716+minsu11@users.noreply.github.com>
2026-01-09 12:34:32 +0900

    [level 1] Title: 숫자 짝꿍, Time: 70.96 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,11 +1,10 @@
 import java.lang.Math;
-import java.util.*;
 class Solution {
     public String solution(String X, String Y) {
         StringBuilder answer = new StringBuilder();
-        List<Integer> list = new ArrayList<>();
         int[] xArr = new int[10];
         int[] yArr = new int[10];
+        int total =0;
         for(char c : X.toCharArray()){
             int i = c - '0';
             xArr[i]++;
@@ -16,28 +15,34 @@ class Solution {
             yArr[i]++;
         }
         
-        for(int i = 0; i < 10; i++){
-            if(xArr[i] ==0 || yArr[i] ==0){
-                continue;
-            }
-            int common = Math.min(xArr[i], yArr[i]);
-            for(int j = 0; j < common; j++){
-                list.add(i);
-            }
+        for(int i = 9; i>=0; i--){
+            total += Math.min(xArr[i],yArr[i] );
         }
-        if(list.size() ==0){
+        
+        if(total ==0){
             return "-1";
         }
-        list.sort(Comparator.reverseOrder());
         
-        for(int i =0; i < list.size(); i++){
-            int num = list.get(i);
-            if(i == 0 && num == 0){
-                return "0";
+        boolean onlyZero = true;
+        for(int i = 9; i>=1; i--){
+            if(Math.min(xArr[i],yArr[i] ) > 0){
+                onlyZero = false;
+                break;
             }
-            answer.append(num);
         }
         
+        if(onlyZero){
+            return "0";
+        }
+        
+        
+        for(int i = 9; i >=0; i--){
+            int common = Math.min(xArr[i], yArr[i]);
+            for(int j = 0; j < common; j++){
+                answer.append(i);
+            }
+        }
+     
         return answer.toString();
     }
 }
