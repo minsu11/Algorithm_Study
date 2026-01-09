b6ff2c41694a0e1215c56f6a7606fa5e59b51669
ms <43956716+minsu11@users.noreply.github.com>
2026-01-09 13:47:14 +0900

    [level 2] Title: 튜플, Time: 78.54 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,43 +1,20 @@
 import java.util.*;
 class Solution {
     public int[] solution(String s) {
+        Set<Integer> set = new HashSet<>();
         List<Integer> list = new ArrayList<>();
-        String[] strArr = s.split("},");
-        if(strArr.length == 1){
-            String str = strArr[0].replace("{","").replace("}","");
-            return new int[]{Integer.parseInt(str)};
-        }
-        
-        for(int i = 0; i < strArr.length;i++){
-            strArr[i] = strArr[i].replace("{","").replace("}","");
-        }
-        Arrays.sort(strArr, Comparator.comparingInt(String::length));
-        
-        StringBuilder sb = new StringBuilder();
-        list.add(Integer.parseInt(strArr[0]));
-        for(int i = 1; i < strArr.length; i++){
-            String[] st = strArr[i].split(",");
-            for(int j = 0; j < st.length; j++){
-                int num = Integer.parseInt(st[j]);
-                boolean check = true;
-                for(int k =0; k < list.size();k++){
-                    if(list.get(k) == num){
-                        check = false;
-                        break;
-                    }
+        String[] strArr = s.replaceAll("[{]"," ").replaceAll("[}]"," ")
+            .trim().split(" , ");
+        Arrays.sort(strArr,Comparator.comparingInt(String::length));
+        int[] answer = new int[strArr.length];
+        int idx = 0;
+        for(String st : strArr){
+            String[] numArr = st.split(",");
+            for(String s2 : numArr){
+                if(set.add(Integer.parseInt(s2))){
+                    answer[idx++] = Integer.parseInt(s2);
                 }
-                if(check){
-                    
-                list.add(num);
-                }
-                
             }
-        }
-        
-        int[] answer = new int[list.size()];
-        
-        for(int i = 0; i < list.size(); i++){
-            answer[i]=list.get(i);
         }
         
         return answer;
