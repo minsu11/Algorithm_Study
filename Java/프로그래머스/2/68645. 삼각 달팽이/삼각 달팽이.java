22bd3bd3febb86904ddb50fd50ac56e4a8996069
ms <43956716+minsu11@users.noreply.github.com>
2026-01-11 14:01:15 +0900

    [level 2] Title: 삼각 달팽이, Time: 11.86 ms, Memory: 146 MB -BaekjoonHub

--- Java/프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ Java/프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,26 +1,18 @@
 class Solution {
     public int[] solution(int n) {
-        
         int size = (n*(n+1))/2;
         int[] answer = new int[size];
         int num = 1;
-        // 방향
         int direction = 3;
-        // 층수
         int n1 = 0;
         int target = n;
         int cnt = 0;
         int idx = 0;
-        // 0
-        // 1 2
-        // 3 4 5
-        // 6 7 8 9
-        // 10 11 12 13 14
-        // 15 16 17 18 19 20
+       
         while(num <= size){
             if(direction% 3 == 0){
                 idx= idx +n1;
-                n1++; // n1 = 1;
+                n1++;
             }else if(direction % 3== 1){
                 idx++;
             }else if(direction % 3 ==2){
