1f1cf8a0a918239ff24f969e07868ba658fd1af4
ms <43956716+minsu11@users.noreply.github.com>
2026-01-13 13:37:00 +0900

    [level 2] Title: [3차] n진수 게임, Time: 10.76 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,23 +1,21 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        // n: 진수, m: 인원, p 순서
-        StringBuilder answer = new StringBuilder();
-        char[] cArr = calcStr(t*m,n).toCharArray();
-        int idx = p -1;
+        StringBuilder answer = new StringBuilder(t);
+        int num = 0;
+        int turn =0;
         while(answer.length() < t){
-            answer.append(String.valueOf(cArr[idx]));
-            idx+=m;
+            String s = Integer.toString(num,n).toUpperCase();
+            
+            for(int i = 0; i < s.length() && answer.length() <t; i++){
+                if( turn % m == (p-1) ){
+                    answer.append(s.charAt(i));
+                }
+                turn++;
+            }
+            
+            num++;
         }
         
         return answer.toString();
-    }
-    
-    public String calcStr(int t, int n){
-        StringBuilder sb =new StringBuilder();
-        for(int i =0; i < t; i++){
-            sb.append(Integer.toString(i,n).toUpperCase());
-            
-        }
-        return sb.toString();
     }
 }
