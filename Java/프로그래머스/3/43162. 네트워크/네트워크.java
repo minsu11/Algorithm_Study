21bec563b2bc04374a897e661579a8cd2892fcaa
ms <43956716+minsu11@users.noreply.github.com>
2026-01-26 10:08:26 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 86.7 MB -BaekjoonHub

--- Java/프로그래머스/3/43162. 네트워크/네트워크.java
+++ Java/프로그래머스/3/43162. 네트워크/네트워크.java
@@ -5,35 +5,23 @@ class Solution {
         int answer = 0;
         // 본인은 1
         boolean[] visited = new boolean[n];
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        
-        for(int i = 0; i < computers.length; i++){
-            List<Integer> list = new ArrayList<>();
-            for(int j = 0; j < computers[i].length; j++){
-                if(i != j && computers[i][j] == 1){
-                    list.add(j);
-                }
-            }
-            map.put(i, list);
-        }
-        
+       
         Deque<Integer> q= new ArrayDeque<>();
         for(int i = 0; i < n; i++){
             if(visited[i]){
                 continue;
             }
             q.add(i);
-            while(q.size()>0)
+            visited[i] = true;
+            while(!q.isEmpty())
             {
                 int cur = q.poll();
-                List<Integer> list = map.get(cur);
-                visited[cur] = true;
-                for(int j = 0; j < list.size(); j++){
-                    int num = list.get(j);
-                    if(visited[num]){
-                        continue;
+                for(int next = 0; next < n; next++){
+                    if (cur == next) continue;
+                    if(!visited[next] && computers[cur][next] ==1){
+                        q.add(next);
+                        visited[next]=true;
                     }
-                    q.add(list.get(j));
                 }
             }
             
