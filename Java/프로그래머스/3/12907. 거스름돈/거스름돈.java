9c08430b51d77608e0d847ff01c8568f7ae097b3
ms <43956716+minsu11@users.noreply.github.com>
2026-02-13 08:19:43 +0900

    [level 3] Title: 거스름돈, Time: 15.73 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,21 @@
+class Solution {
+    // dp 문제
+    public int solution(int n, int[] money) {
+        int answer = 0;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        for(int coin : money){
+            for(int x = 1; x <=n ; x++){
+                if(x - coin >= 0){
+                    
+                    dp[x] = dp[x]+dp[x-coin];
+                }
+            }
+        }
+       
+        
+        return dp[n];
+    }
+    
+    
+}
