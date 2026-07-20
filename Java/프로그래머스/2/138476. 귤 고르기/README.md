ed75fd09ca781143a158ec11e501e7f4d8dc5514
ms <43956716+minsu11@users.noreply.github.com>
2026-07-20 17:49:53 +0900

    [level 2] Title: 귤 고르기, Time: 44.89 ms, Memory: 104 MB -BaekjoonHub

--- Java/프로그래머스/2/138476. 귤 고르기/README.md
+++ Java/프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 107 MB, 시간: 51.93 ms
+메모리: 104 MB, 시간: 44.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 04일 17:18:59
+2026년 07월 20일 17:49:50
 
 ### 문제 설명
 
--- Java/프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ Java/프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,36 +1,75 @@
 import java.util.*;
 class Solution {
     
-    public int solution(int k, int[] tangerine) {
-        int answer = 0;
-        Map<Integer, Integer> freq = new HashMap<>();
-        int size = tangerine.length;
-        int[] cntArr = new int[size+1];
+//     public int solution(int k, int[] tangerine) {
+//         int answer = 0;
+//         Map<Integer, Integer> freq = new HashMap<>();
+//         int size = tangerine.length;
+//         int[] cntArr = new int[size+1];
         
-        for(int x : tangerine){
-            freq.put(x, freq.getOrDefault(x,0) +1);
-        }
+//         for(int x : tangerine){
+//             freq.put(x, freq.getOrDefault(x,0) +1);
+//         }
         
-        for(int c : freq.values()){
-            cntArr[c]++;
-        }
+//         for(int c : freq.values()){
+//             cntArr[c]++;
+//         }
        
-        for(int i = size; i >=0; i--){
-            if(cntArr[i] ==0){
-                continue;
-            }
-            int type = cntArr[i];
-            int total = cntArr[i] * i;
+//         for(int i = size; i >=0; i--){
+//             if(cntArr[i] ==0){
+//                 continue;
+//             }
+//             int type = cntArr[i];
+//             int total = cntArr[i] * i;
             
-            if(total <= k){
-                answer += type;
-                k -= total;
+//             if(total <= k){
+//                 answer += type;
+//                 k -= total;
+//             }
+//             else{
+//                 answer += (k+i-1)/i;
+//                 break;
+//             }
+//         }
+//         return answer;
+//     }
+    public int solution(int k, int[] tangerine) {
+        int answer = 1;
+        // greedy
+        Arrays.sort(tangerine);
+        int num = tangerine[0];
+        int cnt = 0;
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < tangerine.length; i++){
+            if( num == tangerine[i]){
+                cnt++;
+                
+                if(i == tangerine.length-1){
+                    list.add(cnt);
+                }
+            }else{
+                list.add(cnt);
+                cnt = 1;
+                num = tangerine[i];
             }
-            else{
-                answer += (k+i-1)/i;
+            
+        }
+        
+        int[] arr = new int[list.size()];
+        for(int i =0 ; i < list.size(); i++){
+            arr[i] = list.get(i);
+        }
+        Arrays.sort(arr);
+        
+        for(int i = arr.length-1; i>=0; i--){
+            if(k <= arr[i]){
                 break;
+            }else{
+                k -= arr[i];
+                answer+=1;
             }
         }
+        
         return answer;
     }
 }
