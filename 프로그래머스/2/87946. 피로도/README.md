9ba7710efc9086af2c471a9d06b54ab130fc9242
ms <43956716+minsu11@users.noreply.github.com>
2026-09-23 19:03:54 +0900

    [level 2] Title: 피로도, Time: 3.84 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 93.1 MB, 시간: 3.19 ms
+메모리: 77.8 MB, 시간: 3.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 31일 15:37:46
+2026년 09월 23일 19:03:50
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,31 +1,34 @@
 class Solution {
     int max = 0;
+    int[][] dungeons;
+    boolean[] visited;
+    
     public int solution(int k, int[][] dungeons) {
-        int answer = -1;
-        int maxDungeon = dungeons.length;
-        boolean[] visited = new boolean[maxDungeon];
-        dfs(visited, k, dungeons, 0);
+        int size = dungeons.length;
+        visited = new boolean[size];
+        this.dungeons = dungeons;
+        for(int i = 0; i < dungeons.length; i++){
+            dfs(k,i,0);   
+        }
         return max;
     }
     
-    // k 피로도
-    public void dfs(boolean[] visited, int k, int[][] dungeons, int cnt){
-        if(max< cnt){
-            max = cnt;
-        }
-        
-        for(int i = 0; i < dungeons.length; i++){
-            if(visited[i]){
-                continue;
+    public void dfs(int k, int idx, int cnt){
+        if(visited[idx] || dungeons[idx][0] > k){
+            if(max < cnt){
+                max = cnt;
             }
-            if(k < dungeons[i][0]){
-                continue;
-            }
-            visited[i] = true;
-            dfs(visited, k - dungeons[i][1], dungeons, cnt+1);
-
-            visited[i] = false;
+            return;
         }
+        visited[idx] = true;
+        cnt+=1;
+        k -= dungeons[idx][1];
+        for(int i = 0; i < this.dungeons.length; i++){
+            dfs(k, i,cnt);
+        }
+        visited[idx] = false;
         
     }
+    
+  
 }
